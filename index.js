96b68daa2796a71fde1c16594d5aca45f2af36a8
Matthew Hidalgo <mhidalgo83@outlook.com>
2020-06-02 15:49:37 -0700

    initial commit with desktop design complete, need to implement slider functionality

---
+++ index.js
@@ -0,0 +1,10 @@
+const sliderRight = document.querySelector("#slider-arrow-right");
+const sliderLeft = document.querySelector("#slider-arrow-left");
+
+sliderRight.addEventListener("click", (e) => {
+  console.log("this works right");
+});
+
+sliderLeft.addEventListener("click", (e) => {
+  console.log("this works left");
+});
